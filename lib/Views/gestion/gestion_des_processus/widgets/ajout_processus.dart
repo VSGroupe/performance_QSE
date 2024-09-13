0882312f5c5c426321fdad7a0a980f09814cc8b2
eke333 <ehoumanjoel.fr@gmail.com>
2024-09-13 03:33:15 +0000

    Ajout de processus (Okay à 100%). Fiche d'identité des processus (Okay à 40%)
    Le code interagit correctement avec l'API
    Prototype de la fiche d'indenté est Okay à 40%.

--- lib/Views/gestion/gestion_des_processus/widgets/ajout_processus.dart
+++ lib/Views/gestion/gestion_des_processus/widgets/ajout_processus.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert'; // Pour encoder/décoder les données JSON
+import 'package:lottie/lottie.dart'; // Assurez-vous d'ajouter lottie dans vos dépendances pubspec.yaml
 
 class AjoutProcessus extends StatefulWidget {
   const AjoutProcessus({Key? key}) : super(key: key);
@@ -8,16 +11,185 @@ class AjoutProcessus extends StatefulWidget {
 }
 
 class _AjoutProcessusState extends State<AjoutProcessus> {
+
+  final String baseUrl = "http://localhost:5000";
+
+  final _formKey = GlobalKey<FormState>();
+
+  String _typeProcessus = 'Management';
+  String _libelleProcessus = '';
+  String _pilote = '';
+  bool _showSuccessAnimation = false;
+
+  Future<void> _ajouterProcessus() async {
+    if (_formKey.currentState!.validate()) {
+      final url = Uri.parse('$baseUrl/ajouter_processus');
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'type_processus': _typeProcessus,
+          'libelle_processus': _libelleProcessus,
+          'pilote': _pilote,
+        }),
+      );
+
+      if (response.statusCode == 201) {
+        setState(() {
+          _showSuccessAnimation = true;
+          _typeProcessus = 'Management';
+          _libelleProcessus = '';
+          _pilote = '';
+          _formKey.currentState?.reset();
+        });
+
+        // Cacher l'animation après 2 secondes
+        Future.delayed(const Duration(seconds: 2), () {
+          setState(() {
+            _showSuccessAnimation = false;
+          });
+        });
+      } else {
+        final error = jsonDecode(response.body)['error'];
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Erreur: $error')),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Ajouter un processus",
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('Ajouter un Processus'),
+      //   backgroundColor: Colors.deepPurple,
+      // ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 400),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        'Ajouter un nouveau processus',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+
+                      // Sélection du type de processus
+                      DropdownButtonFormField<String>(
+                        value: _typeProcessus,
+                        items: const [
+                          DropdownMenuItem(value: 'Management', child: Text('Management')),
+                          DropdownMenuItem(value: 'Operationnels', child: Text('Opérationnels')),
+                          DropdownMenuItem(value: 'Support', child: Text('Support')),
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            _typeProcessus = value!;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Type de processus',
+                          border: OutlineInputBorder(),
+                          contentPadding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+
+                      // Champ pour le libellé du processus
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Nom du processus',
+                          border: OutlineInputBorder(),
+                          contentPadding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _libelleProcessus = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Veuillez entrer le nom du processus';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+
+                      // Champ pour le pilote du processus
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Responsabilité',
+                          border: OutlineInputBorder(),
+                          contentPadding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _pilote = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Veuillez entrer le rôle de ce processus';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 24),
+
+                      // Bouton pour soumettre le formulaire
+                      ElevatedButton(
+                        onPressed: _ajouterProcessus,
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                        ),
+                        child: const Text('Ajouter'),
+                      ),
+
+                      // Animation de succès après l'ajout réussi
+                      if (_showSuccessAnimation)
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16.0),
+                          child: Lottie.asset(
+                            'assets/animations/success.json', // Assurez-vous d'avoir ce fichier d'animation
+                            width: 200,
+                            height: 150,
+                            repeat: false,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
--- lib/Views/gestion/gestion_des_processus/widgets/fiche_d_identite_processus.dart
+++ lib/Views/gestion/gestion_des_processus/widgets/fiche_d_identite_processus.dart
@@ -10,13 +10,92 @@ class FicheDIdentiteProcessus extends StatefulWidget {
 class _FicheDIdentiteProcessusState extends State<FicheDIdentiteProcessus> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Fiche d'identité des processus",
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            color: Color(0xFF7FC01E),
+            child: const SizedBox(
+              width: 50,
+              height: double.maxFinite,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("F", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("O", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("U", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("R", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("N", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("I", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("S", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("S", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("E", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("U", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("R", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("S", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                ],
+              ),
+            ),
+          ),
+
+          const SizedBox(width: 10,),
+
+          Container(
+            color: Colors.blueGrey,
+            child: SizedBox(
+              width: 800,
+              height: double.maxFinite,
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    color: Colors.orange,
+                    height: 180,
+                    width: double.maxFinite,
+                    child: Row(),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    color: Colors.white,
+                    height: 100,
+                    width: double.maxFinite,
+                    child: Row(),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    color: Colors.green,
+                    height: 180,
+                    width: double.maxFinite,
+                    child: Row(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          const SizedBox(width: 10,),
+
+          Container(
+            color: Color(0xFF7FC01E),
+            child: const SizedBox(
+              width: 50,
+              height: double.maxFinite,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("C", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("L", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("I", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("E", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("N", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("T", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text("S", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
