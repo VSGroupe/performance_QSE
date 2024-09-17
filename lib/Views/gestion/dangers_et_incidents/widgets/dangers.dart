537813e3fc8738f5542056c8b9eb4ab05e57f8c4
eke333 <ehoumanjoel.fr@gmail.com>
2024-09-17 16:56:25 +0000

    Ajout du lient du lien de l'API héberger dans le fichier  danger.tart du sous module Dangers et Incidents

--- lib/Views/gestion/dangers_et_incidents/widgets/dangers.dart
+++ lib/Views/gestion/dangers_et_incidents/widgets/dangers.dart
@@ -16,7 +16,7 @@ class Dangers extends StatefulWidget {
 
 class _DangersState extends State<Dangers> {
 
-  final String baseUrl = "http://localhost:5000"; // URL de l'API Flask
+  final String baseUrl = "http://localhost:5000"; // URL de l'API Flask // https://api-performance-qse.onrender.com
 
   late Future<List<Map<String, dynamic>>> dangersFuture;
 
